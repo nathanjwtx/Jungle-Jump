12bc4cd29f538ebb33d65a33df6728af54775a2a
Nathan <nathanjwtx@nathanjwtx.io>
2019-04-18 18:58:36 -0500

    Added intial FSM to Player. Added variables to code and scene.

--- scenes/player/Player.cs
+++ scenes/player/Player.cs
@@ -3,28 +3,62 @@ using System;
 
 public class Player : KinematicBody2D
 {
-    private string _state;
+
+    [Export]
+    public int RunSpeed;
+    [Export]
+    public int JumpSpeed;
+    [Export]
+    public int Gravity;
+
+    public enum State
+    { IDLE, RUN, JUMP, HURT, DEAD };
+
+    private State _currentState;
     private string _anim;
     private string _newAnim;
 
     public string NewAnim { get => _newAnim; set => _newAnim = value; }
     public string Anim { get => _anim; set => _anim = value; }
-    public string State { get => _state; set => _state = value; }
+    public State CurrentState { get => _currentState; set => _currentState = value; }
 
-    // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        ChangeState("IDLE");
+        ChangeState(State.IDLE);
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
-
-    public void ChangeState(string state)
+    public override void _PhysicsProcess(float delta)
     {
+        base._PhysicsProcess(delta);
+        if (NewAnim != Anim)
+        {
+            Anim = NewAnim;
+            GetNode<AnimationPlayer>("AnimationPlayer").Play(Anim);
+        }
+    }
 
+    public void ChangeState(State newState)
+    {
+        CurrentState = newState;
+        switch (CurrentState)
+        {
+            case State.IDLE:
+                Anim = "idle";
+                break;
+            case State.RUN:
+                Anim = "run";
+                break;
+            case State.HURT:
+                Anim = "hurt";
+                break;
+            case State.JUMP:
+                Anim = "jump_up";
+                break;
+            case State.DEAD:
+                Hide();
+                break;
+            default:
+                break;
+        }
     }
 }
