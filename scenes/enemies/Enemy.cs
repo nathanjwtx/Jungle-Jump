197d5e8ad647605745e84ba759a20b40b17ec6e0
Nathan <nathanjwtx@nathanjwtx.io>
2019-05-06 19:44:35 -0500

    Updated environment collision shapes. Reworked enemy script to ignore slopes

--- scenes/enemies/Enemy.cs
+++ scenes/enemies/Enemy.cs
@@ -8,6 +8,7 @@ public class Enemy : KinematicBody2D
 
     public Vector2 Velocity { get; private set; }
     private int Facing { get; set; }
+    private int NewFacing { get; set; }
     private float NewY { get; set; }
 
     public override void _Ready()
@@ -18,32 +19,29 @@ public class Enemy : KinematicBody2D
     public override void _PhysicsProcess(float delta)
     {
         base._PhysicsProcess(delta);
-        GetNode<Sprite>("Sprite").FlipH = Velocity.x > 0;
+        GetNode<Sprite>("Sprite").FlipH = Facing == 1;
+//        GetNode<Sprite>("Sprite").FlipH = Velocity.x > 0;
         NewY += Velocity.y + Gravity * delta;
         Velocity = new Vector2(Facing * Speed, NewY);
 
         Velocity = MoveAndSlide(Velocity, new Vector2(0, -1));
 
-        // GD.Print($"enemy: {GetNode<CollisionShape2D>("CollisionShape2D").GlobalPosition.y - 28}");
-
         for (int i = 0; i < GetSlideCount(); i++)
         {
             var colliderType = GetSlideCollision(i).GetCollider();
-//            GD.Print(colliderType.ToString());
-            if (colliderType is Player)
+
+            if (colliderType is Player p)
             {
-                if (colliderType != null)
-                {
-                    // ReSharper disable once PossibleInvalidCastException
-                    Player p = (Player) colliderType;
-                    p.Hurt();
-                }
+                p.Hurt();
             }
 
-            if (GetSlideCollision(i).Normal.x != 0)
+            if (colliderType is TileMap t)
             {
-                Facing = Mathf.Sign((int) GetSlideCollision(i).Normal.x);
-                Velocity = new Vector2(Velocity.x, -100);
+                if (t.Name == "Slopes" || GetSlideCollision(i).Normal.x != 0)
+                {
+                    Facing *= -1;
+                    Velocity = new Vector2(Velocity.x, -100);
+                }
             }
         }
 
